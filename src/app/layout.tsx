0d124ef73233573161555f7d34d57691d3d55e6a
mutahirshah11 <mutahirhps@gmail.com>
2024-12-14 13:20:45 +0500

    added analytics in layout for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}
       <Footer/>
+      <Analytics/>
       </body>
     </html>
   );
