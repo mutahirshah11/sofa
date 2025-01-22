d5005d1b6d1d8c5de9c5d809ffc0243fc48d506f
mutahirshah11 <mutahirhps@gmail.com>
2025-01-22 13:54:59 +0500

    changes

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link';
 
 interface buttontype {
   name: string;
@@ -6,11 +7,13 @@ interface buttontype {
 }
 const Button = ({name}:buttontype) => {
   return (
-    <div>
-            <button className="ml-4 px-6 py-2  text-black border-[1px] border-black rounded-lg hover:bg-yellow-200 hover:border-yellow-300 ">
-               {name}
-              </button>
-    </div>
+  <div>
+  <Link href={"../Checkout/"}>
+  <button className="ml-4 px-6 py-2  text-black border-[1px] border-black rounded-lg hover:bg-yellow-200 hover:border-yellow-300 ">
+     {name}
+    </button>
+</Link>
+   </div>
   )
 }
 
